5854f6f5bb3bf79128e90f5ce38557f34b3767b4
Edward Wong <edwardlm.wong@gmail.com>
2020-05-19 20:53:49 -0400

    Full example code.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # deno_rust_wasm
 This shows how you can import Rust WASM files into Deno to speed things up greatly.
+
+// Installation
+
+1. Install rust.
+a. Go to https://www.rust-lang.org/tools/install.
+b. Copy install script.
+c. Run script on machine.
+
+2. Install wasm-pack
+a. Go to https://rustwasm.github.io/wasm-pack/installer/
+b. Copy install script
+c. Run on machine
+
+// Build rwasm
+
+1. cd into rwasm directory.
+2. Run "wasm-pack buld".
+
+// Run server
+
+1. deno run --allow-net --allow-read app.ts
+
+// Test server
+
+1. Deno: curl -d '{"type":"deno","rounds":46340}' -H 'Content-Type: application/json' http://localhost:8000/api
+2. Rust: curl -d '{"type":"rust","rounds":46340}' -H 'Content-Type: application/json' http://localhost:8000/api
---
+++ app.ts
@@ -0,0 +1,71 @@
+import {
+  Application,
+  Router,
+  Request,
+  Response,
+} from "https://deno.land/x/oak/mod.ts";
+
+// max javascript number value = 2147483647. so maxRounds < sqrt(2147483647) = 46340.95
+const maxRounds = 46340;
+
+// wasm
+const wasmCode = await Deno.readFile("./rwasm/pkg/rwasm_bg.wasm");
+const wasmModule = new WebAssembly.Module(wasmCode);
+const wasmInstance = new WebAssembly.Instance(wasmModule);
+const rwasm = wasmInstance.exports;
+
+// app
+const app = new Application();
+
+// routes
+const router = new Router();
+router.post("/api", async (ctx: { request: Request; response: Response }) => {
+  const body = await ctx.request.body();
+  if (
+    body == undefined || body.value.type == undefined ||
+    body.value.rounds == undefined
+  ) {
+    ctx.response.status = 400;
+    ctx.response.body = { error: "Invalid data" };
+    return;
+  }
+
+  const type = body.value.type;
+  const rounds = body.value.rounds;
+
+  if (!(["deno", "rust"].includes(type))) {
+    ctx.response.status = 400;
+    ctx.response.body = { error: "Not 'deno' or 'rust'" };
+    return;
+  }
+
+  if (rounds > maxRounds) {
+    ctx.response.status = 400;
+    ctx.response.body = { error: "Parameter 'rounds' is out of bounds" };
+    return;
+  }
+
+  const scale = 1000000; // nanoseconds
+  const start = performance.now() * scale;
+
+  let count = 0;
+  if (type == "deno") {
+    for (let i = 0; i < rounds; i++) {
+      for (let j = 0; j < rounds; j++) {
+        count += 1;
+      }
+    }
+  } else {
+    count = rwasm.compute(rounds);
+  }
+
+  const duration = performance.now() * scale - start;
+
+  ctx.response.status = 200;
+  ctx.response.body = { type, duration, count };
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Server started @ http://localhost:8000");
+await app.listen({ port: 8000 });
