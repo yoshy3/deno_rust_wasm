5f81a64ef8281300445955e2c1960cb44271d5d1
Edward Wong <edwardlm.wong@gmail.com>
2020-09-19 12:06:08 -0400

    Updated code for Rust v1.46.0, Deno v1.4.1, Oak v6.2.0.

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import {
   Router,
   Request,
   Response,
-} from "https://deno.land/x/oak/mod.ts";
+} from "https://deno.land/x/oak@v6.2.0/mod.ts";
 
 // max javascript number value = 2147483647. so maxRounds < sqrt(2147483647) = 46340.95
 const maxRounds = 46340;
@@ -12,36 +12,34 @@ const maxRounds = 46340;
 const wasmCode = await Deno.readFile("./rwasm/pkg/rwasm_bg.wasm");
 const wasmModule = new WebAssembly.Module(wasmCode);
 const wasmInstance = new WebAssembly.Instance(wasmModule);
-const rwasm = wasmInstance.exports;
+const rwasm: any = wasmInstance.exports;
 
 // app
 const app = new Application();
 
 // routes
 const router = new Router();
-router.post("/api", async (ctx: { request: Request; response: Response }) => {
-  const body = await ctx.request.body();
-  if (
-    body == undefined || body.value.type == undefined ||
-    body.value.rounds == undefined
-  ) {
-    ctx.response.status = 400;
-    ctx.response.body = { error: "Invalid data" };
+router.post("/api", async ({ request, response }: { request: Request, response: Response }) => {
+  const result = request.body();
+  const body = await result.value;
+  if (body == undefined || body.type == undefined || body.rounds == undefined) {
+    response.status = 400;
+    response.body = { error: "Invalid data" };
     return;
   }
 
-  const type = body.value.type;
-  const rounds = body.value.rounds;
+  const type = body.type;
+  const rounds = body.rounds;
 
   if (!(["deno", "rust"].includes(type))) {
-    ctx.response.status = 400;
-    ctx.response.body = { error: "Not 'deno' or 'rust'" };
+    response.status = 400;
+    response.body = { error: "Not 'deno' or 'rust'" };
     return;
   }
 
   if (rounds > maxRounds) {
-    ctx.response.status = 400;
-    ctx.response.body = { error: "Parameter 'rounds' is out of bounds" };
+    response.status = 400;
+    response.body = { error: "Parameter 'rounds' is out of bounds" };
     return;
   }
 
@@ -60,9 +58,8 @@ router.post("/api", async (ctx: { request: Request; response: Response }) => {
   }
 
   const duration = performance.now() * scale - start;
-
-  ctx.response.status = 200;
-  ctx.response.body = { type, duration, count };
+  response.status = 200;
+  response.body = { type, duration, count };
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
